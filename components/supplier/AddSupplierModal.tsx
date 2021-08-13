96332a78710bb8ba50b87915c456ee294717105b
djudjuu <julfaber@gmail.com>
2021-08-13 11:32:19 +0200

    fix add supplier input parameter name

--- components/supplier/AddSupplierModal.tsx
+++ components/supplier/AddSupplierModal.tsx
@@ -24,7 +24,7 @@ export const AddSupplierModal = () => {
         axiosInstance.post(
             "/v1/supplier/new",
             {
-                supplier: {
+                input: {
                     id: receiverInfo.id,
                     name: receiverInfo.name,
                     defaultTerms: {
